035e6184a4af1d3f5ffe4c3fe548008fc0b75a73
Unknown <bkehoe@irobot.com>
2018-02-24 09:28:35 -0500

    Versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+def find_version(name):
+    import os.path, codecs, re
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, name), 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name='file-transformer',
-    version='0.1.0',
+    version=find_version('file_transformer.py'),
     description='Helper main for executables that transform files and/or stdin/stdout',
     py_modules=['file_transformer'],
     author='Ben Kehoe',
     author_email='bkehoe@irobot.com',
     project_urls={
-        "https://github.com/benkehoe/file-transformer",
+        "Source code": "https://github.com/benkehoe/file-transformer",
     },
     license='Apache Software License 2.0',
     classifiers=(
@@ -18,6 +29,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: Apache Software License',
     ),
 )
